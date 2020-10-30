63261251e23a223287f4e9dc78885039ffe9ab1b
Nina Hwang <nihwang12@gmail.com>
2020-10-29 23:17:17 -0700

    Added modal

--- components/memeMaker/memeMaker.jsx
+++ components/memeMaker/memeMaker.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, ScrollView, Image, StyleSheet, TextInput, View, Dimensions, Button } from 'react-native';
+import { Text, ScrollView, Image, StyleSheet, TextInput, View, Dimensions, Button, Modal, TouchableHighlight } from 'react-native';
 import { getMemeImg } from '../../api/memeApi';
 import MultiSelect from 'react-native-multiple-select'
 
@@ -73,6 +73,42 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 16,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
 });
 
@@ -83,7 +119,8 @@ class MemeMaker extends Component {
       randomImg: '',
       topText: '',
       bottomText: '',
-      selectedItems: []
+      selectedItems: [],
+      successModal: false,
     };
   }
 
@@ -99,12 +136,41 @@ class MemeMaker extends Component {
     this.setState({ selectedItems }, () => console.warn('Selected Items: ', selectedItems))
   }
 
+  onButtonSubmit = () => {
+    this.setState({
+      successModal: true
+    })
+  }
+
 
   render() {
-    const { selectedItems } = this.state
+    const { selectedItems, successModal } = this.state
 
     return (
       <ScrollView>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={successModal}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Postcards sent!</Text>
+
+              <TouchableHighlight
+                style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                onPress={() => {
+                  this.setState({successModal: false});
+                }}
+              >
+                <Text style={styles.textStyle}>Exit Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
         <View>
           <Image
             source={{uri: this.state.randomImg.url}}
@@ -153,13 +219,15 @@ class MemeMaker extends Component {
           </View>
         </View>
         <View style={{backgroundColor: 'black'}}>
-          <Button 
+          <Button
             style={{
               marginTop: 24,
               padding: 8,
             }}
             color="white"
-            title="Send postcards!">
+            title="Send postcards!"
+            onPress={this.onButtonSubmit}
+            >
           </Button>
         </View>
       </ScrollView>
